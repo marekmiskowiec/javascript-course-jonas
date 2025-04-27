64b40049a182324378b42a796ccf9a2f56dda226
Marek Tomasz Miśkowiec <miskowiec.ma@gmail.com>
2025-04-27 06:13:13 +0200

    Data Types

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,8 +1,17 @@
-// 1. Values and variables
-let country = "Poland",
-  continent = "Europe",
-  population = 38;
+// // 1. Values and variables
+// let country = "Poland",
+//   continent = "Europe",
+//   population = 38;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// // 2. Data Types
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,6 +1,24 @@
-let js = "amazing";
+// // 9. Variable name conventions
+// let jonas_matilda = "JM";
+// let $function = 27;
+// let person = "jonas";
+// let PI = 3.1415;
 
-let firstName = "Mark";
-let lastName = "Kowalski";
+// let myFirstsJob = "Coder";
+// let myCurrentJob = "Teacher";
 
-if (lastName === "Kowalski") console.log("Kowalski");
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+// // 11. Data Types
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+
+// javascriptIsFun = "YES!";
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
